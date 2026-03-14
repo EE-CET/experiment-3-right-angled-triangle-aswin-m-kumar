3c0b8b0cfd14249cc94f5d656acf40eb586990f6
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-03-14 23:14:17 +0530

    Fix pattern output format

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,7 +2,11 @@ public class Triangle {
     public static void main(String[] args) {
         for (int i=1; i <= 5; i+=2) {
             for (int j=1; j<=i; j++) {
-                System.out.print("*");
+                if (j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("* ");
+                }
             }
             System.out.println();
         }
