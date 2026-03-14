8a07206577a888b9c851c39825d1a1b09e864190
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-03-14 23:09:55 +0530

    Corrected the error

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,7 +2,7 @@ public class Triangle {
     public static void main(String[] args) {
         for (int i=1; i <= 5; i+=2) {
             for (int j=1; j<=i; j++) {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
